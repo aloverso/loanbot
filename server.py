08cee75cad7300a9ff0980db6f982784eac5fe92
Anne LoVerso <aelv13@gmail.com>
2017-03-27 14:07:50 -0400

    checkout flow chart

--- server.py
+++ server.py
@@ -18,9 +18,9 @@ tools = db.tools
 
 # stages of checkout process
 NO_CONTACT = 0
-GREETING = 1
+SENT_GREETING = 1
 WANT_CHECKOUT = 2
-IDENTIFY_TOOL = 3
+#IDENTIFY_TOOL = 3
 CONFIRM_TOOL = 4
 HOW_LONG = 5
 CLOSING = 6
@@ -29,6 +29,7 @@ class User:
     def __init__(self, sender_id):
         self.sender_id = sender_id
         self.tools = []
+        self.temp_tools = []
         self.stage = NO_CONTACT
 
 class Tool:
@@ -74,7 +75,8 @@ def posthook():
 
                         user = make_or_find_user(sender_id)
 
-                        determine_response_and_send(user, message_text)
+                        updated_user = determine_response_and_send(user, message_text)
+                        users.find_one_and_replace({"sender_id":sender_id}, updated_user)
 
                     elif 'attachments' in messaging_event["message"]:
 
@@ -101,16 +103,115 @@ def determine_response_and_send(user, message):
     print(user)
     print(user['stage'])
 
-    if any(word in message for word in checkout_words):
+    if user['stage'] == NO_CONTACT:
+        # check for checkout words
+        if any(word in message for word in checkout_words):
+            # id as checkout request
+            user['stage'] = WANT_CHECKOUT
+        else:
+            # send greeting and ask what tool
+            send_message(user['sender_id'], "Hi there! I'm the loan bot, what tool would you like to check out?")
+            user['stage'] = SENT_GREETING
+            return user
+
+    if user['stage'] == WANT_CHECKOUT:
+        find_tools()
+
+    # we sent them a greeting and asked what tool
+    if user['stage'] == SENT_GREETING:
+        find_tools()
+
+    if user['stage'] == CONFIRM_TOOL:
+        if message == 'yes':
+            # for tool in temp_tools:
+            #     user['tools'].append(tool)
+            time = calculate_time_for_loan(user['temp_tools'])
+            send_quickreply_message(user['sender_id'], "Great! Is a loan time of {} days okay?".format(time))
+            user['stage'] = HOW_LONG
+            return user
+        if message == 'no':
+            send_message(user['sender_id'], "Sorry I misunderstood.  What tool do you want to check out?")
+            user['temp_tools'] = []
+            user['stage'] = WANT_CHECKOUT
+            return user
+
+    if user['stage'] == HOW_LONG:
+        tool_string = ''
+        for tool in temp_tools:
+            user['tools'].append(tool)
+            tool_string = tool_string + " and " # allow for a list of tools
+        # remove final and from string
+        tool_string = tool_string[:-5]
+        user['temp_tools'] = []
+
+        # TODO: update tool's due date with response
+
+        send_message(user['sender_id'], "You're all set!  I'll remind you to return the {} before it's due.".format(tool_string))
+        return user
+
+    ## TODO: check for cancelling
+
+    def find_tools():
         found_tool = False
+        # TODO: find more than one tool
         for tool in tools:
             if tool in message:
                 found_tool = True
                 send_quickreply_message(user['sender_id'], "Sounds like you want to check out a {}, is that correct?".format(tool))
+                user['stage'] = CONFIRM_TOOL
+                user['temp_tools'].append(tool)
+                return user
         if not found_tool:
             send_message(user['sender_id'], "What tool do you want to check out?")
-    else:
-        send_quickreply_message(user['sender_id'], "Hi, I'm the loan bot, would you like to check out a tool?")
+            user['stage'] = WANT_CHECKOUT
+            return user
+
+    # what format for time (start: day)
+    def calculate_time_for_loan(temp_tools):
+        return 1
+
+    # if any(word in message for word in checkout_words):
+    #     found_tool = False
+    #     for tool in tools:
+    #         if tool in message:
+    #             found_tool = True
+    #             send_quickreply_message(user['sender_id'], "Sounds like you want to check out a {}, is that correct?".format(tool))
+    #     if not found_tool:
+    #         send_message(user['sender_id'], "What tool do you want to check out?")
+    # else:
+    #     send_quickreply_message(user['sender_id'], "Hi, I'm the loan bot, would you like to check out a tool?")
+
+def send_howlong_quickreply_message(recipient_id, message_text):
+    params = { "access_token": PAGE_ACCESS_TOKEN }
+    headers = { "Content-Type": "application/json" }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text,
+            "quick_replies":[
+                {
+                    "content_type":"text",
+                    "title":"yes",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_YES"
+                },
+                {
+                    "content_type":"text",
+                    "title":"12 hours instead",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_12_HOURS"
+                }
+                {
+                    "content_type":"text",
+                    "title":"3 days instead",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD_3_DAYS"
+                }
+            ]
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code, r.text)
 
 def send_quickreply_message(recipient_id, message_text):
     params = { "access_token": PAGE_ACCESS_TOKEN }
