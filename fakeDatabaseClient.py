b505ef635308e0cf583dbc90fff6b1ea17806105
Mimi <mimi@students.olin.edu>
2017-04-12 16:32:32 -0400

    tests created and travis setup

---
+++ fakeDatabaseClient.py
@@ -0,0 +1,120 @@
+import os
+
+'''
+the class sructure used for the User in the Mongo database
+'''
+tools_list = [
+{
+	"_id":{
+		"$oid": "58e5528dbf24551abe30660f"
+	},
+	"current_user": {
+		"$oid": "58ebabe3638acc000b2e2429"
+	},
+	"name": "screwdriver",
+	"current_due_date": 1492100080
+},
+{
+    "_id": {
+        "$oid": "58e5528dbf24551abe306610"
+    },
+    "current_due_date": 1491424505,
+    "name": "drill",
+    "current_user": {
+        "$oid": "58dfe761db78bb000bf7c88b"
+    }
+},
+{
+    "_id": {
+        "$oid": "58e5528dbf24551abe306611"
+    },
+    "name": "arduino",
+    "current_due_date": None,
+    "current_user": None
+}]
+
+users_list = [
+{
+    "_id": {
+        "$oid": "58ebabe3638acc000b2e2429"
+    },
+    "sender_id": "1346430625424620",
+    "tools": [
+        {
+            "$oid": "58e5528dbf24551abe30660f"
+        }
+    ],
+    "stage": 0,
+    "temp_tools": []
+}]
+
+class User:
+    def __init__(self, sender_id):
+        self.sender_id = sender_id
+        self.tools = []
+        self.temp_tools = []
+        self.stage = 0
+
+'''
+A client which connects to Mongo and deals with Mongo database operations
+'''
+class fakeDatabaseClient():
+	'''
+	fake version of get_all_users
+	'''
+	def get_all_users(self):
+		return users_list
+
+	'''
+	fake version of get_all_tools
+	'''
+	def get_all_tools(self):
+		return tools_list
+
+	'''
+	fake version of find_user
+	'''
+	def find_user(self, field_name, field_value):
+		return None
+
+	'''
+	fake version of find_user_by_sender_id
+	'''
+	def find_user_by_sender_id(self, sender_id):
+		return None
+
+	'''
+	fake version of find_or_create_user
+	'''
+	def find_or_create_user(self, sender_id):
+	    return users_list[0]
+
+	'''
+	given an updated user dictionary with the same sender_id,
+	replaces the old database entry with the new one
+	'''
+	def update_user(self, updated_user):
+		sender_id = updated_user['sender_id']
+		self.users.find_one_and_replace({"sender_id":sender_id}, updated_user)
+
+	'''
+	finds a tool by searching on the name field
+	'''
+	def find_tool_by_name(self, name):
+		return None
+		# return self.tools.find_one({'name':name})
+
+	'''
+	finds a tool by searching on the id field
+	'''
+	def find_tool_by_id(self, tool_id):
+		return None
+		# return self.tools.find_one({'_id':tool_id})
+
+	'''
+	given an updated tool dictionary with the same _id,
+	replaces the old database entry with the new one
+	'''
+	def update_tool(self, updated_tool):
+		return None
+		# self.tools.find_one_and_replace({"_id":updated_tool['_id']}, updated_tool)
--- tests.py
+++ tests.py
@@ -2,10 +2,12 @@
 import unittest
 
 from server import app
-
-def add(a, b):
-  return a+b
-
+import conversationHandler
+import fakeDatabaseClient
+import messengerClient
+# messenger_client = messengerClient.MessengerClient()
+database_client = fakeDatabaseClient.fakeDatabaseClient()
+conversation_handler = conversationHandler.ConversationHandler(database_client)
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -15,9 +17,33 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(3, 4), 7)
+    def test_find_tool_names_in_message(self):
+        test_message_1 = "I would like to borrow the drill and the screwdriver, please!"
+        test_message_2 = "I want to take out a drill"
+        test_message_3 = "I think it's cool that the library has tools"
+        self.assertEqual(conversation_handler.find_tool_names_in_message(test_message_1), [{
+    "_id":{
+        "$oid": "58e5528dbf24551abe30660f"
+    },
+    "current_user": {
+        "$oid": "58ebabe3638acc000b2e2429"
+    },
+    "name": "screwdriver",
+    "current_due_date": 1492100080
+},
+{
+    "_id": {
+        "$oid": "58e5528dbf24551abe306610"
+    },
+    "current_due_date": 1491424505,
+    "name": "drill",
+    "current_user": {
+        "$oid": "58dfe761db78bb000bf7c88b"
+    }
+}])
+        self.assertEqual(len(conversation_handler.find_tool_names_in_message(test_message_2)),1)
+        self.assertEqual(len(conversation_handler.find_tool_names_in_message(test_message_3)),0)
+
 
 if __name__ == '__main__':
     unittest.main()
